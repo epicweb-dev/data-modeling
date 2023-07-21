10afcc6f2153b49c12f881d9877c5103e7d5b02a
Kent C. Dodds <me+github@kentcdodds.com>
2023-07-21 15:08:02 -0600

    finish all material

---
+++ exercises/08.sql/02.problem.validation/app/routes/users+/index.tsx
@@ -0,0 +1,84 @@
+import { json, redirect, type DataFunctionArgs } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { GeneralErrorBoundary } from '~/components/error-boundary.tsx'
+import { SearchBar } from '~/components/search-bar.tsx'
+import { prisma } from '~/utils/db.server.ts'
+import { cn, getUserImgSrc, useDelayedIsSubmitting } from '~/utils/misc.ts'
+
+export async function loader({ request }: DataFunctionArgs) {
+	const searchTerm = new URL(request.url).searchParams.get('search')
+	if (searchTerm === '') {
+		return redirect('/users')
+	}
+
+	const like = `%${searchTerm ?? ''}%`
+	const users = await prisma.$queryRaw`
+		SELECT user.id, user.username, user.name
+		FROM User AS user
+		WHERE user.username LIKE ${like}
+		OR user.name LIKE ${like}
+		LIMIT 50
+	`
+
+	return json({ status: 'idle', users } as const)
+}
+
+export default function UsersRoute() {
+	const data = useLoaderData<typeof loader>()
+	const isSubmitting = useDelayedIsSubmitting({
+		formMethod: 'GET',
+		formAction: '/users',
+	})
+
+	return (
+		<div className="container mb-48 mt-36 flex flex-col items-center justify-center gap-6">
+			<h1 className="text-h1">Epic Notes Users</h1>
+			<div className="w-full max-w-[700px] ">
+				<SearchBar status={data.status} autoFocus autoSubmit />
+			</div>
+			<main>
+				{data.status === 'idle' ? (
+					// @ts-expect-error 🦺 we'll fix this next
+					data.users.length ? (
+						<ul
+							className={cn(
+								'flex w-full flex-wrap items-center justify-center gap-4 delay-200',
+								{ 'opacity-50': isSubmitting },
+							)}
+						>
+							{/* @ts-expect-error 🦺 we'll fix this next */}
+							{data.users.map(user => (
+								<li key={user.id}>
+									<Link
+										to={user.username}
+										className="flex h-36 w-44 flex-col items-center justify-center rounded-lg bg-muted px-5 py-3"
+									>
+										<img
+											alt={user.name ?? user.username}
+											src={getUserImgSrc(user.imageId)}
+											className="h-16 w-16 rounded-full"
+										/>
+										{user.name ? (
+											<span className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-center text-body-md">
+												{user.name}
+											</span>
+										) : null}
+										<span className="w-full overflow-hidden text-ellipsis text-center text-body-sm text-muted-foreground">
+											{user.username}
+										</span>
+									</Link>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>No users found</p>
+					)
+				) : null}
+			</main>
+		</div>
+	)
+}
+
+export function ErrorBoundary() {
+	return <GeneralErrorBoundary />
+}
